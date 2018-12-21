d82c86b98233323ece86cacc8f42ef9a20e83a9a
tmgreenside <trevmg7@gmail.com>
2018-12-20 19:00:51 -0800

    Added setup file

--- GUMAD_CTF/setup.sh
+++ GUMAD_CTF/setup.sh
@@ -1,6 +1,4 @@
-# This script installs requirements to run this site.
 pip3 install -r requirements.txt;
-cd GUMAD_CTF;
 python3 manage.py makemigrations;
 python3 manage.py migrate;
 python3 manage.py runserver;
