5e2ea6ea3e25553161c5f549bac27260e65f1ace
mdulin2 <mdulin2@zagmail.gonzaga.edu>
2018-12-24 02:58:08 -0500

    Added gitnore file

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 # This script installs requirements to run this site.
+
+sudo apt-get install mysql-server
+sudo apt-get install libmysqlclient-dev
+
 pip3 install -r requirements.txt;
 cd GUMAD_CTF;
 python3 manage.py makemigrations;
